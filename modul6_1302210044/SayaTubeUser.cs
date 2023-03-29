ee8776106c59ab1fdbfdf683ffd03994c2c96cf4
shieldadchr <127294977+shieldadchr@users.noreply.github.com>
2023-03-29 17:05:21 +0700

    menambahkan 2 class

---
+++ modul6_1302210044/SayaTubeUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_1302210044
+{
+    internal class SayaTubeUser
+    {
+    }
+}
